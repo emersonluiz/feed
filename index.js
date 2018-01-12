78b70ad602b1cb6af584162aa3e13ce6b46f3eb5
Emerson Luiz de Castro <erms.castro@gmail.com>
2018-01-12 09:54:53 -0200

    Create project

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var request = require('request');
+var parseString = require('xml2js').parseString;
+
+const app = express();
+
+const url1 = 'http://www.gazetadopovo.com.br/rss/economia/nova-economia';
+const url2 = 'http://www.gazetadopovo.com.br/agronegocio/rss/ultimas-noticias';
+const url3 = 'https://economia.awesomeapi.com.br/json/all';
+
+app.use(express.static('./public_html/'));
+
+var rss = [];
+
+var transform = function(xml, res) {
+    parseString(xml, (err, result) => {
+        var json = result;
+        for(var i=0; i<json.rss.channel[0].item.length; i++) {
+            var obj = {image:"", title:"", description:""};
+            if(json.rss.channel[0].item[i].image != null) {
+                if(json.rss.channel[0].item[i].image[0].url == undefined) {
+                    url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0]);
+                } else {
+                    url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0].url[0]);
+                }
+            }
+            obj.image = url;
+            obj.title = (json.rss.channel[0].item[i].title != undefined)?json.rss.channel[0].item[i].title:"";
+            obj.description = (json.rss.channel[0].item[i].description != undefined)?json.rss.channel[0].item[i].description:"";
+            rss.push(obj);
+        }
+        console.log(result);
+        res.send(JSON.stringify(rss));
+        return
+    });
+}
+
+app.get('/noticias/economia', (req, res) => {
+    request.get(url1, (err, response) => {
+        transform(response.body, res)
+        return
+    })
+})
+
+app.get('/noticias/agronegocio', (req, res) => {
+    request.get(url2, (err, response) => {
+        transform(response.body, res)
+        return
+    })
+})
+
+app.get('/cotacao', (req, res) => {
+    request.get(url3, (err, response) => {
+        console.log(response)
+        res.send(response.body);
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server on line');
+})
---
+++ public_html/feed-script.js
@@ -0,0 +1,87 @@
+function jsonGET(arquivo){
+	
+	arquivo += (arquivo.indexOf("?") != -1)?"&cacheDate="+new Date():"?cacheDate="+new Date();
+	if (window.XMLHttpRequest) {
+		var lerXML=new XMLHttpRequest();
+	}else {
+		var lerXML = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	lerXML.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 401) {
+		   redirect();
+		}
+	};
+
+	lerXML.open("GET",arquivo,false);
+    //lerXML.setRequestHeader("Authorization","Bearer " + token);
+    
+    //lerXML.setRequestHeader('Access-Control-Allow-Origin', '*');
+    ///lerXML.setRequestHeader('Content-Type', 'application/xml');
+
+
+	lerXML.send();
+
+	var xmlDoc = lerXML.responseText;
+
+	return xmlDoc;
+}
+
+let rss = ["agronegocio", "economia"];
+cotation();
+
+let i = 0;
+let json = load(i);
+i++;
+
+let time = 30000;
+let index = 0;
+index = noticias(index);
+
+let intervalo = window.setInterval(function() {
+	index = noticias(index);
+}, time);
+
+function cotation() {
+	let a = jsonGET("/cotacao");
+	let cot = JSON.parse(a);
+
+	document.getElementById("cotation").innerHTML = "<h3>Dólar Comercial: R$ " + cot.USD.ask + 
+													"&nbsp;&nbsp;&nbsp;&nbsp; Euro: R$ " + cot.EUR.ask + 
+													"&nbsp;&nbsp;&nbsp;&nbsp; Peso Argentino: R$ " + cot.ARS.ask + 
+													"&nbsp;&nbsp;&nbsp;&nbsp; Bitcoin: R$ " + cot.BTC.ask + "</h3>";
+}
+
+function load(ind) {
+	let a = jsonGET("/noticias/"+rss[ind]);
+	return JSON.parse(a);
+}
+
+function noticias(index) {
+
+	if((json.length-1) < index) {
+		index = 0;
+		json = load(i);
+		cotation();
+		if(i == 1) {
+			i = 0;
+		} else {
+			i++;
+		}
+	}
+
+	let url = "";
+	if(json[index].image != null) {
+		document.body.style.backgroundImage = "url('" + json[index].image +"')";
+		document.body.style.backgroundPosition = "center  top";
+		document.body.style.backgroundSize = "100% auto";
+	} else {
+		document.body.style.background = "#6495ED";
+	}
+	
+	if(json[index].description != "") {
+		document.getElementById("legend").innerHTML = "<h1 style='color: #FFF; font-size: 40px'>"+json[index].title+"</h1>"+
+													  "<h2 style='color: #FFF; font-size: 28px'>"+json[index].description+"</h2>";
+	}
+	return ++index;
+}
