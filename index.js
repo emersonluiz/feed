8da2e95a5386ce120ff810075465cd4d284a91b4
Emerson <erms.castro@gmail.com>
2019-07-23 10:49:15 -0300

    Fix Bug

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var parseString = require('xml2js').parseString;
 
 const app = express();
 
-const url1 = 'http://www.gazetadopovo.com.br/rss/economia/nova-economia';
-const url2 = 'http://www.gazetadopovo.com.br/agronegocio/rss/ultimas-noticias';
+//const url1 = 'http://www.gazetadopovo.com.br/rss/economia/nova-economia';
+//const url2 = 'http://www.gazetadopovo.com.br/agronegocio/rss/ultimas-noticias';
+const url1 = 'https://www.gazetadopovo.com.br/feed/rss/economia.xml'
+const url2 = 'https://www.gazetadopovo.com.br/feed/rss/agronegocio.xml';
 const url3 = 'https://economia.awesomeapi.com.br/json/all';
 
 app.use(express.static('./public_html/'));
@@ -15,19 +17,23 @@ var rss = [];
 var transform = function(xml, res) {
     parseString(xml, (err, result) => {
         var json = result;
-        for(var i=0; i<json.rss.channel[0].item.length; i++) {
-            var obj = {image:"", title:"", description:""};
-            if(json.rss.channel[0].item[i].image != null) {
-                if(json.rss.channel[0].item[i].image[0].url == undefined) {
-                    url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0]);
-                } else {
-                    url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0].url[0]);
+        try {
+            for(var i=0; i<json.rss.channel[0].item.length; i++) {
+                var obj = {image:"", title:"", description:""};
+                if(json.rss.channel[0].item[i].image != null) {
+                    if(json.rss.channel[0].item[i].image[0].url == undefined) {
+                        url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0]);
+                    } else {
+                        url = encodeURI("http://www.gazetadopovo.com.br" + json.rss.channel[0].item[i].image[0].url[0]);
+                    }
                 }
+                obj.image = url;
+                obj.title = (json.rss.channel[0].item[i].title != undefined)?json.rss.channel[0].item[i].title:"";
+                obj.description = (json.rss.channel[0].item[i].description != undefined)?json.rss.channel[0].item[i].description:"";
+                rss.push(obj);
             }
-            obj.image = url;
-            obj.title = (json.rss.channel[0].item[i].title != undefined)?json.rss.channel[0].item[i].title:"";
-            obj.description = (json.rss.channel[0].item[i].description != undefined)?json.rss.channel[0].item[i].description:"";
-            rss.push(obj);
+        } catch(err) {
+            console.log("Can't parser");
         }
         console.log(result);
         res.send(JSON.stringify(rss));
